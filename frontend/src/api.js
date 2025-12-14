c4e00b78db1a338acfecf7edcd07df8118f8c390
vamshi5654 <bandarivamshi07@gmail.com>
2025-12-14 21:07:53 +0530

    Initial commit: MERN e-commerce app

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL || "https://e-commerce-backend-1yu8.onrender.com/api",
-// });
-
-// export default API; // ✅ This line is mandatory
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",  // 🔥 LOCAL BACKEND
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
-export default API;
+export default api;
--- frontend/src/components/protectedroute.jsx
+++ frontend/src/components/protectedroute.jsx
@@ -1,17 +1,14 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  // ✅ Must match the same key used in login.jsx
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem("userInfo"));
 
-  // 🚫 If no user found → redirect to login
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // ✅ If logged in → show the requested page
   return children;
 };
 
 export default ProtectedRoute;
+  
